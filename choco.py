df2ad2f599e5d0b19c2562b864e8ae489f451da4
Luciano Larrea <takticflow@gmail.com>
2023-03-11 14:17:26 -0300

    Estado:Funcionando

--- choco.py
+++ choco.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get('/')
+async def index():
+    return{'estado','funcionando'}
+
 @app.get('/puro')
 async def puro():
     return{'puro','chocolate con 100% de cacao'}
